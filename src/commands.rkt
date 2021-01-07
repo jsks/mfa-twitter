98ca78bdb69666a0e0cafff0c4dac461dfeee751
Joshua Krusell <js.shirin@gmail.com>
2021-01-07 16:55:01 +0100

    Split commands into separate file and add scan-tweets
    The sub-command `scan-tweets` downloads at most 30.000 existing tweet
    IDs from postgres and checks if any have been deleted on twitter. If
    not, it updates the engagement counts (ie favorite and retweet
    counts).

---
+++ src/commands.rkt
@@ -0,0 +1,129 @@
+#lang racket/base
+
+(require json
+         racket/date
+         racket/format
+         racket/contract
+         racket/list
+         racket/match
+         racket/sequence
+         racket/string
+         threading)
+
+(require "database.rkt"
+         "thread-pool.rkt"
+         "twitter.rkt"
+         "utils.rkt")
+
+(provide
+ max-threads
+ (contract-out [dispatch (-> string? hash? void?)]))
+
+(date-display-format 'iso-8601)
+
+(define (dispatch command args)
+  (access-token (hash-ref args 'access_token))
+  (init-db #:user (hash-ref args 'pg_user)
+           #:password (hash-ref args 'pg_password)
+           #:database (hash-ref args 'pg_database))
+
+  (case command
+    [("help") (handle-help)]
+    [("sync-timelines") (handle-sync-timelines)]
+    [("scan-tweets") (handle-scan-tweets)]
+    [("rate-status") (handle-rate-status)]
+    [else
+     (displayln (format "Unknown command: ~a, see 'mfa help'" command)
+                (current-error-port))
+     (exit 1)]))
+
+(define (handle-help)
+  (displayln #<<EOF
+Available sub-commands:
+help               Show this message.
+sync-timelines     Update timeline store for all accounts in database.
+scan-tweets        Check if any stored tweets have been deleted and update
+                     engagement numbers
+rate-status        List current rate limits
+EOF
+             ))
+
+;;; Sync Timelines
+
+(define (handle-sync-timelines)
+  (for/thread ([account (in-list (get-accounts))])
+    (match-let ([(vector screen_name user_id since_id) account])
+      (with-handlers
+        ([exn:fail? (λ (e) (log-error "@~a ~a" screen_name (exn-message e)))])
+        (let ([total (process-timeline user_id since_id)])
+          (when (> total 0)
+            (log-info "@~a -> downloaded ~a tweet(s)" screen_name total)))))))
+
+(define (process-timeline user_id since_id)
+  (call-with-bound-transaction
+   (λ ()
+     (for/sum ([tweets (in-producer (get-timeline user_id #:since_id since_id) (void))])
+       (for ([tweet (in-list tweets)])
+         (insert-tweet (prune tweet))
+         (update-engagement (select tweet 'id)
+                            (select tweet 'favorite_count)
+                            (select tweet 'retweet_count)))
+       (length tweets)))))
+
+(define (prune tweet)
+  (for/hash ([(key value) (in-hash tweet)]
+             #:when (not (regexp-match #rx"_str$" (symbol->string key))))
+    (match key
+      [(or 'retweeted_status 'quoted_status) (values key (prune value))]
+      ['user (values 'user_id (hash-ref value 'id))]
+      ['source (values key (regexp-replace* "<[^>]+>" value ""))]
+      [_ (values key value)])))
+
+;;; Scan tweets
+
+;; TODO: log number of deleted tweets
+(define (handle-scan-tweets)
+  (let ([n (~> (rate-limit-status '("statuses"))
+               (select 'resources 'statuses '/statuses/lookup 'remaining)
+               (* 100))])
+    (if (= n 0) (log-error "Rate limit exceeded")
+        (log-info "Updating ~a tweets" n))
+    (for/thread ([tweet-ids (in-slice 100 (get-tweet-ids n))])
+      (with-handlers
+        ([exn:fail? (λ (e) (log-error (exn-message e)))])
+        (call-with-bound-transaction (λ () (process-batch tweet-ids)))))))
+
+(define (process-batch tweet-ids)
+  (let ([tweets (get-tweets-by-id tweet-ids)])
+    (for ([(k tweet) (in-hash (select tweets 'id))])
+      (let ([id (symbol->number k)])
+        (touch-tweet id)
+        (cond [(eq? tweet (json-null)) (set-tweet-deleted id)]
+              [else (update-engagement id
+                                       (select tweet 'favorite_count)
+                                       (select tweet 'retweet_count))])))))
+
+;;; Rate limit status
+
+(define (handle-rate-status)
+  (define resources (rate-limit-status '("statuses" "application")))
+  (define endpoints '(/statuses/user_timeline
+                      /statuses/lookup
+                      /application/rate_limit_status))
+
+  (define max-endpoint-len
+    (apply max (map (λ (sym) (string-length (symbol->string sym))) endpoints)))
+
+  (define (extract-api-family sym)
+    (string->symbol (first (string-split (symbol->string sym) "/"))))
+
+  (for ([endpoint (in-list endpoints)])
+    (let ([l (select resources 'resources (extract-api-family endpoint) endpoint)])
+      (printf "~a : ~a\n" (~a endpoint #:min-width max-endpoint-len)
+              (format-rate-limit l)))))
+
+(define (format-rate-limit tbl)
+    (match tbl
+      [(hash-table ('limit limit) ('remaining remaining) ('reset reset))
+       (format "~a expires ~a" (~a remaining "/" limit #:min-width 10)
+               (date->string (seconds->date reset) #t))]))
--- src/main.rkt
+++ src/main.rkt
@@ -1,61 +1,34 @@
 #lang racket/base
 
 (require racket/cmdline
-         racket/function
-         racket/list
-         racket/match
          racket/runtime-path)
 
-(require "config.rkt"
-         "database.rkt"
-         "logging.rkt"
-         "thread-pool.rkt"
-         "twitter.rkt")
+(require "commands.rkt"
+         "config.rkt"
+         "logging.rkt")
 
 (define-runtime-path default-cred-file "../.env")
 
 (define cred-file (make-parameter default-cred-file))
 (define log-level (make-parameter 'info))
 
-(define command
-  (command-line
-   #:program "mfa"
-   #:once-each
-   [("-c" "--credential-file") file
-    "Location of credential file [default: .env]" (cred-file file)]
-   [("-l" "--log-level") level
-    "Verbosity level for log messages [default: info]" (log-level (string->symbol level))]
-   [("-n" "--num-threads") num
-    "Maximum number of concurrent threads [default: 4]" (max-threads (string->number num))]))
+(module+ main
+  (define command
+    (command-line
+     #:program "mfa"
+     #:once-each
+     [("-c" "--credential-file") file
+      "Location of credential file [default: .env]"
+      (cred-file file)]
+     [("-l" "--log-level") level
+      "Verbosity level for log messages [default: info]"
+      (log-level (string->symbol level))]
+     [("-n" "--num-threads") num
+      "Maximum number of concurrent threads [default: 4]"
+      (max-threads (string->number num))]
+     #:ps "Available sub-commands: help, sync-timelines, scan-tweets, rate-status"
+     #:args (sub-command)
+     sub-command))
 
-(init-logger (log-level))
-(define config-args (load-config (cred-file)))
-
-(access-token (hash-ref config-args 'access_token))
-(init-db #:user (hash-ref config-args 'pg_user)
-         #:password (hash-ref config-args 'pg_password)
-         #:database (hash-ref config-args 'pg_database))
-
-(define (prune tweet)
-  (for/hash ([(key value) (in-hash tweet)]
-             #:when (not (regexp-match #rx"_str$" (symbol->string key))))
-    (match key
-      [(or 'retweeted_status 'quoted_status) (values key (prune value))]
-      ['user (values 'user_id (hash-ref value 'id))]
-      ['source (values key (regexp-replace* "<[^>]+>" value ""))]
-      [_ (values key value)])))
-
-(define (get-and-process-tweets account)
-  (match-let ([(vector screen_name user_id since_id) account])
-    (with-handlers
-      ([exn:fail? (λ (e) (log-error "@~a ~a" screen_name (exn-message e)))])
-      (call-with-bound-transaction
-       (λ ()
-         (let ([tweet-generator (get-timeline user_id #:since_id since_id)])
-           (for ([tweets (in-producer tweet-generator)]
-                 #:break (not tweets))
-             (log-info "@~a -> downloaded ~a tweet(s)" screen_name (length tweets))
-             (insert-tweets (map prune tweets)))))))))
-
-(for/thread ([account (in-list (get-accounts))])
-  (get-and-process-tweets account))
+  (init-logger (log-level))
+  (dispatch command (load-config (cred-file))))
