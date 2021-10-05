647bdca4e1bfd5635a9940cae5c5fc1f3b3eea28
Joshua Krusell <js.shirin@gmail.com>
2021-10-05 22:08:56 +0200

    Update tests for unified db-stats function
    This slightly changes the behaviour of checking for latest/checked
    tweets in that for the former we now use added rather than the twitter
    creation timestamp and for the latter we no longer filter out tweets
    added within the last seven days.

--- test/database.rkt
+++ test/database.rkt
@@ -16,6 +16,21 @@
            #:database "postgres"
            #:socket #f)
 
+  (define db-conn
+    (postgresql-connect #:user "test" #:password "postgres" #:database "postgres"))
+
+  (define (n-tweets)
+    (hash-ref (db-stats) 'n-tweets))
+
+  (define (n-deleted)
+    (hash-ref (db-stats) 'n-deleted))
+
+  (define (n-latest-tweets)
+    (apply + (hash-values (hash-ref (db-stats) 'latest-tweets))))
+
+  (define (n-checked-tweets)
+    (apply + (hash-values (hash-ref (db-stats) 'checked-tweets))))
+
   (check-eq? (length (get-accounts)) 2)
   (check-eq? (n-tweets) 2)
 
@@ -23,53 +38,68 @@
     (string->jsexpr
      (format "{\"id\":300,\"user_id\":1,\"created_at\":\"~a\"}"
              (date->string (current-date)))))
-  (insert-tweet tweet)
 
-  (check-eq? (n-tweets) 3)
-  (check-equal? (sort (get-tweet-ids 3) <) '(1))
-  (check-equal? (activity) #(1 0))
-  (check-equal? (user-stats) '(#("a" 1)))
+  (test-case
+      "Insert tweet"
+   (insert-tweet tweet)
 
-  (touch-tweet 1)
-  (check-equal? (activity) #(1 1))
+    (check-eq? (n-tweets) 3)
+    (check-equal? (sort (get-tweet-ids 3) <) '(1)))
 
-  (check-eq? (n-deleted) 0)
-  (set-tweet-deleted 1)
-  (check-eq? (n-deleted) 1)
-  (check-eq? (get-tweet-ids 3) '())
+  (test-case
+      "Check latest/checked tweets, and top user"
+    (check-eq? (n-latest-tweets) 2)
+    (check-eq? (n-checked-tweets) 2)
+    (check-equal? (hash-ref (db-stats) 'top-user) #hash(("a" . 1)))
 
-  (check-exn
-   exn:fail?
-   (λ ()
-     (call-with-bound-transaction
-      (λ ()
-        (insert-tweet (string->jsexpr "{\"id\":99,\"user_id\":1}"))
-        (raise (exn:fail "fail" (current-continuation-marks)))))))
-  (check-eq? (n-tweets) 3)
+    (touch-tweet 1)
+    (check-eq? (n-latest-tweets) 2)
+    (check-eq? (n-checked-tweets) 3))
 
-  (define db-conn
-    (postgresql-connect #:user "test" #:password "postgres" #:database "postgres"))
+  (test-case
+      "Set a tweet as deleted from twitter"
+    (check-eq? (n-deleted) 0)
+    (set-tweet-deleted 1)
+    (check-eq? (n-deleted) 1)
+    (check-eq? (get-tweet-ids 3) '()))
 
-  (check-equal?
-   (query-value db-conn @~a{select json from tweets where tweet_id = 300})
-   tweet)
+  (test-case
+      "Insert invalid tweet"
+    (check-exn
+     exn:fail?
+     (λ ()
+       (call-with-bound-transaction
+        (λ ()
+          (insert-tweet (string->jsexpr "{\"id\":99,\"user_id\":1}"))
+          (raise (exn:fail "fail" (current-continuation-marks)))))))
+    (check-eq? (n-tweets) 3))
 
-  (check-true
-   (query-value
-    db-conn @~a{select added > current_date from tweets where tweet_id = 300}))
+  (test-case
+      "Test updating likes/retweet stats"
+    (update-engagement 2 3 0)
+    (check-equal? (query-row db-conn "select * from engagement") #(2 3 0))
 
-  (check-true
-   (query-value
-    db-conn @~a{select last_checked > current_date from tweets where tweet_id = 300}))
+    (update-engagement 2 5 10)
+    (check-equal? (query-row db-conn "select * from engagement") #(2 5 10)))
 
-  (check-true
-   (query-value
-    db-conn @~a{select last_checked > current_date from tweets where tweet_id = 1}))
+  (test-case
+      "Test retrieving tweet"
+    (check-equal?
+     (query-value db-conn @~a{select json from tweets where tweet_id = 300})
+     tweet))
 
-  (update-engagement 2 3 0)
-  (check-equal? (query-row db-conn "select * from engagement") #(2 3 0))
+  (test-case
+      "Check database tweet timestamps"
+    (check-true
+     (query-value
+      db-conn @~a{select added > current_date from tweets where tweet_id = 300}))
 
-  (update-engagement 2 5 10)
-  (check-equal? (query-row db-conn "select * from engagement") #(2 5 10))
+    (check-true
+     (query-value
+      db-conn @~a{select last_checked > current_date from tweets where tweet_id = 300}))
+
+    (check-true
+     (query-value
+      db-conn @~a{select last_checked > current_date from tweets where tweet_id = 1})))
 
   (disconnect db-conn))
