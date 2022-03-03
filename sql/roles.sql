3e29464304f46c586f37185a145bbad09669d3af
Joshua Krusell <js.shirin@gmail.com>
2022-03-03 15:17:12 +0100

    Grant permissions to profile tables to app user

--- sql/roles.sql
+++ sql/roles.sql
@@ -2,7 +2,8 @@ do $$
 begin
     if not exists (select from pg_catalog.pg_roles where rolname = 'mfa') then
         create role mfa login noinherit;
-        grant insert, update, select on tweets, engagement to mfa;
+        grant insert, update, select on tweets, engagement, profiles, friends to mfa;
+        grant usage, select on sequence profiles_profile_id_seq to mfa;
         grant select on accounts, full_tweets to mfa;
     else
         raise notice 'Role "mfa" already exists, skipping';
